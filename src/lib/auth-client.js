0fb72959b69f43721f3d78f0dc77d53a716eb8da
Anwar Hossain <cseanwar@gmail.com>
2026-06-22 22:21:09 +0200

    Modified again auth-client since I got that windows is not defined

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL:
-    process.env.NEXT_PUBLIC_APP_URL ||
-    window.location.origin,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL
 });
 
 export const { signUp, signIn, signOut, useSession } = authClient;
